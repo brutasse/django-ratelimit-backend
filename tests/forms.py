2dd43ab32a4eef91d3700e966fb48f6696ed0409
Jody McIntyre <scjody@modernduck.com>
2017-08-16 15:55:47 -0400

    Add tests for no_username and new backend

--- tests/forms.py
+++ tests/forms.py
@@ -26,3 +26,30 @@ class CustomAuthForm(Form):
 
     def get_user(self):
         return self.user_cache
+
+
+class TokenOnlyAuthForm(Form):
+    token = CharField(max_length=30)
+
+    def __init__(self, request=None, *args, **kwargs):
+        self.request = request
+        self.user_cache = None
+        super(TokenOnlyAuthForm, self).__init__(*args, **kwargs)
+
+    def clean_token(self):
+        token = self.cleaned_data.get('token')
+
+        # This is NOT how a token-only authentication system should work, but
+        # it allows us to simulate one for testing relatively easily.
+        username, password = token.split('_')
+        self.user_cache = authenticate(username=username,
+                                       password=password,
+                                       request=self.request)
+        if self.user_cache is None:
+            raise ValidationError("Invalid")
+        elif not self.user_cache.is_active:
+            raise ValidationError("Inactive")
+        return self.cleaned_data
+
+    def get_user(self):
+        return self.user_cache
